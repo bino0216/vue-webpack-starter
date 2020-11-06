b73a94d1b53ae01957d81852153b35e1e9243427
bino <bonoself@gmail.com>
2020-11-06 22:00:44 +0900

    start

---
+++ README.md
@@ -0,0 +1,7 @@
+# Template
+새로운 웹 앱 시작할 때 편리하게 하기 위해 만든 템플릿
+
+# 구성
+- Vue 2
+- Vuetify
+- Webpack 4
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+
+const VueLoaderPlugin   = require("vue-loader/lib/plugin");
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const dist_path = __dirname + '/www';
+
+// webpack.config.js
+module.exports = {
+    mode : 'development',
+    watch: true,
+    entry: {
+      main : './src/main.js',
+    },
+    resolve: {
+      extensions: [".js", ".vue"],
+      alias: {
+        '@': path.resolve('src')
+      },
+    },
+    output: {
+      path: dist_path
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.(sa|sc)ss$/,
+            use: [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+              test: /\.vue$/,
+              loader: "vue-loader"
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'file-loader',
+            options: {
+              name: 'files/[hash:10].[ext]'
+            }
+          }
+        ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new htmlWebpackPlugin({
+          template: path.join(__dirname, 'src/index.template.html'),
+          inject: false,
+          filename: path.join(dist_path, '/index.html'),
+          minify: {
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true
+          }
+      })
+    ],
+};
